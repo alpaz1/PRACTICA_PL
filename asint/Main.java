4e40e8b4ad6dcef49de058311bc51c8345469ce9
alpaz1 <alpaz@ucm.es>
2023-03-16 20:17:55 +0100

    Primera version del analizador sintáctico

---
+++ asint/Main.java
@@ -0,0 +1,15 @@
+package asint;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import alex.AnalizadorLexicoTiny;
+
+public class Main {
+   public static void main(String[] args) throws Exception {
+     Reader input = new InputStreamReader(new FileInputStream(args[0]));
+	 AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
+	 asint.parse();
+ }
+}   
