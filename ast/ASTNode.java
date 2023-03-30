3ef26fdfceff5cf45cd68e0b6626d6f709ec2dd7
alpaz1 <alpaz@ucm.es>
2023-03-30 09:20:24 +0200

    Limpieza de código y arreglos de 3 fallos menores

--- ast/ASTNode.java
+++ ast/ASTNode.java
@@ -3,16 +3,11 @@ package ast;
 public abstract class ASTNode {
 
 
-    //public ASTNode nodoVinculo; // nodo con el que se vinculan
-    //public TipoClass tipo;  // tipo de su nodo
-    //public int etiqueta; // etiqueta
+    public NodeKind tipo;  // tipo de su nodo
     
     public ASTNode(){}
 
-    //public abstract void vincular();
-    //public abstract void chequea();
-    //public abstract void calculos(); 
-    //public abstract void generaCodigo(); 
+
     //public abstract NodeKind nodeKind();
     //public abstract String toString();
 }
--- ast/Programa.java
+++ ast/Programa.java
@@ -12,25 +12,12 @@ public class Programa extends ASTNode {
     
     public static Boolean okTipos;
 
-    // Nombres Tipos
-   // public static List<String> tipos_basicos = new ArrayList<String>();
-    //public static List<EnumClass> tipos_enum = new ArrayList<EnumClass>();
-    //public static List<StructClass> tipos_struct = new ArrayList<StructClass>();
-
     
 
     public Programa(DefinitionsList l){//, Fmain fmain) {
         this.definiciones = l;
         //this.fmain = fmain;
         
-        /* 
-        tipos_basicos.add("int");
-        tipos_basicos.add("float");
-        tipos_basicos.add("bool");
-        tipos_basicos.add("void");
-        tipos_basicos.add("char");
-        tipos_basicos.add("string");
-        */
     }
     
     
